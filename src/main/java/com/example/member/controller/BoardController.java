65be7a7bd8f84a6b3333a28869c329bba0be1779
Lee JaeHyeong <leejh7830@naver.com>
2022-05-09 23:38:41 +0900

    220509

---
+++ src/main/java/com/example/member/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.example.member.controller;
+
+
+import com.example.member.entity.Board;
+import com.example.member.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+@RequestMapping("board")
+public class BoardController {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @GetMapping("findall")
+    public String findall(Model model) {
+//        boardRepository.save(
+//                new Board(1L,"aatitle","aacontent", LocalDate.now().toString()));
+        List<Board> list = boardRepository.findAll();
+
+        model.addAttribute("list",list);
+        return "board/findall";
+    }
+}
